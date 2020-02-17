a89cd746811663882c347b1d8092a50a76ebc799
edusnchezj <eduardsnchez@gmail.com>
2020-02-17 11:23:09 +0100

    generadorCartas.py

---
+++ __init__/generadorCartas.py
@@ -0,0 +1,32 @@
+def generadorCartas():
+    import random
+    c = open('../carta.xml', 'w+')
+
+    # Asignamos los nombres a las cartas
+    nombres = ['Pargilana','Coloclan','Canirme','Curgo','Calión','Bele','Histis','Marmenta','Conen','Voldole','Mille','Perofora','Estabie','Numba','Desego','Descierta','Elerochan','Recone','Esmador','Hembad','Almen']
+    # Asignamos los tipos de cada carta
+    tipos = ['Infantería', 'Caballería', 'Lanceros']
+    # Asignamos las distintas descripciones. Después, cada asignación será aleatoria.
+    descripciones = ['¡Por la marea!', 'En su corazón, esta el amor.', '¡Por los dioses que me dieron este poder!', 'Nadie es mejor que yo...', 'Indeseable bestia...', '¡Fuera de mi camino!', '¡Por el poder del agua!']
+
+    c.writelines('<cartas>\n')
+    for a in range(1, 21):
+        # El número de la lista saldrá aleatoriamente asi asignado el valor de ese número a la descripción y al tipo.
+        descripcion = descripciones[random.randint(0, len(descripciones)-1)]
+        tipo = tipos[random.randint(0, len(tipos) - 1)]
+        # Asignamos aleatoriamente los ataques y defensas.
+        ataque = random.randint(0,5)
+        defensa = random.randint(0,5)
+        # Sumamos los ataques y defensas para la invocación. Si es mayor a 5, se mantendrá como 5.
+        invocacion = ataque + defensa
+        if invocacion > 5:
+            invocacion = 5
+        c.write('\t<carta>\n'
+                '\t\t<nombre>{}</nombre>\n'
+                '\t\t<descripcion>{}</descripcion>\n'
+                '\t\t<pts_invocacion>{}</pts_invocacion>\n'
+                '\t\t<ataque>{}</ataque>\n'
+                '\t\t<defensa>{}</defensa>\n'
+                '\t\t<tipo>{}</tipo>\n'
+                '\t</carta>\n'.format(nombres[a], descripcion, invocacion, ataque, defensa, tipo))
+    c.writelines('</cartas>\n')

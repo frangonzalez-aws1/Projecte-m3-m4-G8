4f3202f3bb4fdf8e4b73f1775eb75149f9fc200d
edusnchezj <eduardsnchez@gmail.com>
2020-02-17 12:03:05 +0100

    generadorCartas.py

--- __init__/leerXML.py
+++ __init__/leerXML.py
@@ -1,1 +1,8 @@
-p
+def leerXML():
+    import xml.etree.ElementTree as ET
+    tree = ET.parse('../carta.xml')
+    root = tree.getroot()
+    for a in root.findall('carta/*'):
+        print(a.attrib)
+
+leerXML()

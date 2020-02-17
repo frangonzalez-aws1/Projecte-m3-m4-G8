f3e9952aafc2916b5791a2c55a9fdb47043b1f21
edusnchezj <eduardsnchez@gmail.com>
2020-02-17 12:07:09 +0100

    Leer el XML creado con xPATH

--- __init__/leerXML.py
+++ __init__/leerXML.py
@@ -2,7 +2,7 @@ def leerXML():
     import xml.etree.ElementTree as ET
     tree = ET.parse('../carta.xml')
     root = tree.getroot()
-    for a in root.findall('carta/*'):
+    for a in root.findall('carta/'):
         print(a.attrib)
 
 leerXML()

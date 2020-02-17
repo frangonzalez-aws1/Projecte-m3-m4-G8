81b1646a41e0128be0bad417806fa2d21b60355d
frangonzalez <tim3afgonza@gmail.com>
2020-02-17 12:10:29 +0100

    menu

--- menu.py
+++ menu.py
@@ -1,3 +1,20 @@
 def menu ():
     while True:
-        print ()
+        print ("1.Cargar Cartas\n"
+               "2.Cargar Cartas Enemigo\n"
+               "3.Salir")
+        while True:
+            try:
+                opcion = int(input("Escoge una opcion"))
+                break
+            except ValueError:
+                print("Debe ser un numero entero")
+        if opcion==1:
+            pass
+        elif opcion==2:
+            pass
+        elif opcion==3:
+            break
+        else:
+            print("Opcion no valida")
+menu()

c02c4ad0a4d616cbaa6bd3cc10fce1e763f9579e
edusnchezj <eduardsnchez@gmail.com>
2020-02-18 14:25:01 +0100

    Arreglando el XML

--- modulo/leerXML.py
+++ modulo/leerXML.py
@@ -1,9 +1,12 @@
 def leerXML(xml):
     import xml.etree.ElementTree as ET
-    tree = ET.parse(xml)
-    root = tree.getroot()
-    xmlDic = {}
-    for c in root.findall('carta/*'):
-        xmlDic
+
+    estructura_xml = ET.parse(xml)
+
+    # Obtiene el elemento raíz:
+    raiz = estructura_xml.getroot()
+
+    for elemento_hijo in raiz:
+        print(elemento_hijo)
 
 leerXML('../carta.xml')

5531b76fda85e3e7c0b7a3cc358bc126d35ea76b
edusnchezj <eduardsnchez@gmail.com>
2020-03-02 13:29:07 +0100

    Asignando mazos

--- modulo/crearMazos.py
+++ modulo/crearMazos.py
@@ -3,33 +3,30 @@ def crearMazo(tipo, archivo):
     # Mostramos un error si el atributo <tipo> no es el deseado con la lista creada en la anterior linea.
     assert tipo in types, 'El atributo insertado no es el correcto.'
 
-    mazos = {}
+    cont = 0
+    c = 10
+
+    dic = {1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}, 8: {}, 9: {}, 10: {}}
+    types = ['name', 'description', 'attack', 'defense', 'summonPoints', 'type']
 
     if tipo is 'attack':
-        c = 0
-        card = 1
-        for child in archivo.findall('deck/card/'):
-            mazos[card] = 'p'
-            print(child.tag, child.text)
-            if c == 3:
-                print('---')
-                c = 0
-                card += 1
-                if card == 11:
-                    break
-            else:
-                c += 1
+        for child in archivo.findall('deck/'):
+            print(child.tag, child.value)
     elif tipo is 'defend':
-        print('Defensa:', archivo)
+        i = 0
+        for child in archivo.findall('deck/card/'):
+            dic[1][types[i]] = child.text
+            i += 1
+            print(dic)
     elif tipo is 'random':
         print('Random:', archivo)
+
     elif tipo is 'balanced':
         print('Balanced:', archivo)
 
-    print(mazos)
 
 import xml.etree.ElementTree as ET
-archivoA = ET.parse('../myBaraja.xml')
-archivoA = archivoA.getroot()
-# Elegir tipo de mazo
-crearMazo('attack', archivoA)
+
+archivoa = ET.parse('../myBaraja.xml')
+archivoa = archivoa.getroot()
+crearMazo('defend', archivoa)

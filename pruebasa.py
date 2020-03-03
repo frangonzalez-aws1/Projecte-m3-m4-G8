4af6657ca754eb67b7d04d1e7d72955ecb4576d1
frangonzalez <tim3afgonza@gmail.com>
2020-03-03 13:59:03 +0100

    pruebas mazos

--- pruebasa.py
+++ pruebasa.py
@@ -1,20 +1,38 @@
+import random
 def crearMazo(tipo, archivo):
     types = ['attack', 'defend', 'random', 'balanced']
     # Mostramos un error si el atributo <tipo> no es el deseado con la lista creada en la anterior linea.
     assert tipo in types, 'El atributo insertado no es el correcto.'
-    cont=0
-    c=10
-    dic={1:{}, 2:{}, 3:{}, 4:{}, 5:{}, 6:{}, 7:{}, 8:{}, 9:{}, 10:{}}
+    dic={1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}, 8: {}, 9: {}, 10: {}}
+
+    cnt = 1
+    for child in archivo.findall('deck/card/'):
+        if cnt == 11:
+            break
+        if child.tag == 'name':
+            dic[cnt]['name'] = child.text
+
+        elif child.tag == 'description':
+            dic[cnt]['description'] = child.text
+
+        elif child.tag == 'attack':
+            dic[cnt]['attack'] = child.text
+
+        elif child.tag == 'defense':
+            dic[cnt]['defense'] = child.text
+            cnt += 1
+
+    cnt2 = 1
+    for child in archivo.findall('deck/card'):
+        if cnt2 == 11:
+            break
+        dic[cnt2]['summonPoints'] = child.attrib['summonPoints']
+        dic[cnt2]['type'] = child.attrib['type']
+        cnt2 += 1
+
     if tipo is 'attack':
-       for child in archivo.findall('deck/'):
-            print(child.tag, child.value)
+       print(dic)
     elif tipo is 'defend':
-        for child in archivo.findall('deck/card/'):
-            print(child.text)
-                if int(child2.text) == 5:
-                    dic[cont]=('deck/card/'+child)
-            cont+=1
-            print(cont)
         print(dic)
     elif tipo is 'random':
         print('Random:', archivo)
@@ -24,4 +42,5 @@ def crearMazo(tipo, archivo):
 import xml.etree.ElementTree as ET
 archivoa = ET.parse('./myBaraja.xml')
 archivoa = archivoa.getroot()
+
 crearMazo('defend', archivoa)
